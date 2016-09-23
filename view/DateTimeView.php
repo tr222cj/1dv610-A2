028013cd3c5387855e07e7ea9d1f7e6f136f70ba
Tomas Vaedelund - tr222cj <tr222cj@student.lnu.se>
2016-09-23 16:36:53 +0200

    Fixed a bug where I had hardcoded th after day

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -1,6 +1,6 @@
 <p>
     <?php
     date_default_timezone_set("Europe/Stockholm");
-    echo date('l, \t\h\e d\t\h \o\f F Y, \T\h\e \t\i\m\e \i\s H:i:s');
+    echo date('l, \t\h\e dS \o\f F Y, \T\h\e \t\i\m\e \i\s H:i:s');
     ?>
 </p>
