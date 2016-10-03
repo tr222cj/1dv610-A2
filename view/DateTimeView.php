689da30303376df5d0c7c3b499e93efa52edf46d
Tomas Vaedelund - tr222cj <tr222cj@student.lnu.se>
2016-10-03 13:20:11 +0200

    Fixed a nasty bugg where day always would be shown with two positions...

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -1,6 +1,6 @@
 <p>
     <?php
     date_default_timezone_set("Europe/Stockholm");
-    echo date('l, \t\h\e dS \o\f F Y, \T\h\e \t\i\m\e \i\s H:i:s');
+    echo date('l, \t\h\e jS \o\f F Y, \T\h\e \t\i\m\e \i\s H:i:s');
     ?>
 </p>
